eda778068b9ceae2971515b6f951788f1641b115
kasperwikman <kasperwikmanhki@gmail.com>
2026-01-14 15:32:07 +0200

    lecture 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Car car = new Car("Renault");
+        Car car2 = new Car("BMW");
+
+        System.out.println(car.getName());
+
+        car.setName("Volvo");
+
+        System.out.println(car.getName());
+        System.out.println(car2.getName());
+
     }
 }
