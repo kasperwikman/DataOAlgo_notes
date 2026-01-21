46bf6adb0137304f9fd32040bcea1149d8ff4eef
kasperwikman <kasperwikmanhki@gmail.com>
2026-01-21 11:48:12 +0200

    Calculator class, multiply method and unit tests

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double multiply(double num1, double num2, double num3) {
+        return multiply(num1, num2) * num3;
+    }
+}
---
+++ test/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testMultiplyTwoNumbers() {
+
+        assertEquals(6.0, calculator.multiply(2.0, 3.0));
+    }
+
+    @Test
+    void testMultiplyThreeNumbers() {
+        assertEquals(16.0, calculator.multiply(2.0, 2.0, 4.0));
+    }
+}
